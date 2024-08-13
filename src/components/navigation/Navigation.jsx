1bfa06489b6bbf6587c2a42bb1aeeb1c9adcfb73
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-13 14:42:19 +0200

    Add Home, Shop and Cart to Navigation

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,7 +1,13 @@
+import Home from '../home/Home';
+import Shop from '../shop/Shop';
+import Cart from '../cart/Cart';
+
 const Navigation = () => {
   return (
     <>
-      <h1>Navigation</h1>
+      <Home />
+      <Shop />
+      <Cart />
     </>
   );
 };
