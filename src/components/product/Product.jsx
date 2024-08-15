ac4da34ba2c250b9c9419b0a4d581bc7cfd82cb0
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-15 07:59:15 +0200

    Add `quantity` to Product state

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,12 +1,21 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const Product = ({ product, setCart }) => {
+  const [quantity, setQuantity] = useState(1);
+
   return (
     <>
       <div className="product">
         <img src={product.imageUrl} alt={`image of ${product.title}`} />
         <div>{product.title}</div>
-        <input type="number" id="quantity" defaultValue={1} min={1} />
+        <input
+          type="number"
+          id="quantity"
+          min={1}
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+        />
         <button
           onClick={() =>
             setCart((cart) => {
