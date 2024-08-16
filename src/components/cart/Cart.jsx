3fc571927b584f144516bb38711c03a324439e7c
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-16 15:34:20 +0200

    Update style of Cart

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,4 +1,5 @@
 import CartItem from '../cartItem/CartItem';
+import styles from './Cart.module.css';
 
 const Cart = ({
   cart,
@@ -6,25 +7,46 @@ const Cart = ({
   getProductById,
   removeItemFromCart,
 }) => {
+  if (cart.length === 0) {
+    return <div>Your shopping cart is empty</div>;
+  }
+
   return (
     <>
       <h1>Cart</h1>
-      {cart.map((item) => {
-        return (
-          <CartItem
-            key={item.id}
-            product={getProductById(item.id)}
-            updateItemInCart={updateItemInCart}
-            initialQuantity={item.quantity}
-            removeItemFromCart={removeItemFromCart}
-          />
-        );
-      })}
-      {cart.length > 0 && (
-        <button onClick={() => alert('Thanks for shopping with us!')}>
-          Check out
-        </button>
-      )}
+      <div className={styles.cart}>
+        <div className={styles.cartItems}>
+          {cart.map((item) => {
+            return (
+              <CartItem
+                key={item.id}
+                product={getProductById(item.id)}
+                updateItemInCart={updateItemInCart}
+                initialQuantity={item.quantity}
+                removeItemFromCart={removeItemFromCart}
+              />
+            );
+          })}
+        </div>
+        <div className={styles.cartSummary}>
+          <div>Cart Summary</div>
+          <div>
+            Total: (
+            {cart.reduce((total, cartItem) => total + cartItem.quantity, 0)}{' '}
+            items)
+          </div>
+          <div>
+            {cart.reduce((total, cartItem) => {
+              const subTotal =
+                cartItem.quantity * getProductById(cartItem.id).price;
+              return total + subTotal;
+            }, 0)}
+          </div>
+          <button onClick={() => alert('Thanks for shopping with us!')}>
+            Check out
+          </button>
+        </div>
+      </div>
     </>
   );
 };
