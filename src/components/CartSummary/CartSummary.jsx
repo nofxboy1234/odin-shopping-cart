24c84880a039e5346d289c57e9dfa923c49caf3e
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-16 16:02:28 +0200

    Refactor Cart
    Create a CartSummary component

---
+++ src/components/CartSummary/CartSummary.jsx
@@ -0,0 +1,30 @@
+import styles from './CartSummary.module.css';
+
+const CartSummary = ({ cart, getProductById }) => {
+  return (
+    <>
+      <div className={styles.cartSummary}>
+        <div>Cart Summary</div>
+        <div>
+          Total: (
+          {cart.reduce((total, cartItem) => total + cartItem.quantity, 0)}{' '}
+          items)
+        </div>
+        <div>
+          {cart
+            .reduce((total, cartItem) => {
+              const subTotal =
+                cartItem.quantity * getProductById(cartItem.id).price;
+              return total + subTotal;
+            }, 0)
+            .toFixed(2)}
+        </div>
+        <button onClick={() => alert('Thanks for shopping with us!')}>
+          Check out
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default CartSummary;
