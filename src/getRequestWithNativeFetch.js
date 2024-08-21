e8e7587417fe4c3215acc3c5effc475cb15c2685
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-21 10:48:19 +0200

    Add version of getRequestWithNativeFetch using async

--- src/getRequestWithNativeFetch.js
+++ src/getRequestWithNativeFetch.js
@@ -1,12 +1,20 @@
-const getRequestWithNativeFetch = (url) => {
-  return new Promise((resolve, reject) => {
-    fetch(url).then((res) => {
-      if (!res.ok) {
-        reject(new Error('server error'));
-      }
-      resolve(res.json());
-    });
-  });
+const getRequestWithNativeFetch = async (url) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error('server error');
+  }
+  return res.json();
 };
+
+// const getRequestWithNativeFetch = (url) => {
+//   return new Promise((resolve, reject) => {
+//     fetch(url).then((res) => {
+//       if (!res.ok) {
+//         reject(new Error('server error'));
+//       }
+//       resolve(res.json());
+//     });
+//   });
+// };
 
 export default getRequestWithNativeFetch;
