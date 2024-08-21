70b67fc5e85bcd41f62da36932ae24c5e039b409
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-21 16:17:31 +0200

    Update css

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -14,48 +14,48 @@ const Product = ({ product }) => {
   return (
     <>
       <div className={styles.product}>
-        <div className={styles.productContent}>
-          <img
-            src={product.image}
-            alt={`image of ${product.title}`}
-            width="100px"
-            className={styles.productImage}
-          />
-          <div>{product.title}</div>
-          <div>Price: {product.price.toFixed(2)}</div>
-          <input
-            type="number"
-            id="quantity"
-            min={1}
-            value={quantity}
-            onChange={(e) => {
-              const newValue = Number(e.target.value);
-              setQuantity(Number(newValue));
+        {/* <div className={styles.productContent}>
+          </div> */}
+        <img
+          src={product.image}
+          alt={`image of ${product.title}`}
+          width="100px"
+          className={styles.productImage}
+        />
+        <div>{product.title}</div>
+        <div>Price: {product.price.toFixed(2)}</div>
+        <input
+          type="number"
+          id="quantity"
+          min={1}
+          value={quantity}
+          onChange={(e) => {
+            const newValue = Number(e.target.value);
+            setQuantity(Number(newValue));
 
-              if (isInCart) {
-                updateItemInCart({ id: product.id, quantity: newValue });
-              }
-            }}
-          />
-          <button
-            onClick={() => {
-              if (isInCart) {
-                removeItemFromCart(product.id);
-                setIsInCart(false);
-                setQuantity(1);
-              } else {
-                const quantityInCart = addItemToCart({
-                  id: product.id,
-                  quantity: quantity,
-                });
-                setIsInCart(true);
-                setQuantity(quantityInCart);
-              }
-            }}
-          >
-            {isInCart ? 'Remove from Cart' : 'Add to Cart'}
-          </button>
-        </div>
+            if (isInCart) {
+              updateItemInCart({ id: product.id, quantity: newValue });
+            }
+          }}
+        />
+        <button
+          onClick={() => {
+            if (isInCart) {
+              removeItemFromCart(product.id);
+              setIsInCart(false);
+              setQuantity(1);
+            } else {
+              const quantityInCart = addItemToCart({
+                id: product.id,
+                quantity: quantity,
+              });
+              setIsInCart(true);
+              setQuantity(quantityInCart);
+            }
+          }}
+        >
+          {isInCart ? 'Remove from Cart' : 'Add to Cart'}
+        </button>
       </div>
     </>
   );
