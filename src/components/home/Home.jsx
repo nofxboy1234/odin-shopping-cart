970a678a54ed8ec931ed9b0a18f977b2506ee2dc
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-26 09:59:21 +0200

    Add WrapperTitle

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,17 @@
 import styles from './Home.module.css';
 import Button from '../styledButton/StyledButton';
+import { Wrapper, Title } from '../wrapperTitle/WrapperTitle';
 
 const Home = () => {
   return (
     <>
       <h1 className={styles.heading}>Welcome to Shopping Cart!</h1>
       <Button $primary={true} onClick={() => console.log('Button!')}>
-        I'm a Button
+        I&apos;m a Button
       </Button>
+      <Wrapper>
+        <Title>Hello World!</Title>
+      </Wrapper>
     </>
   );
 };
---
+++ src/components/wrapperTitle/WrapperTitle.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #bf4f74;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+export { Wrapper, Title };
