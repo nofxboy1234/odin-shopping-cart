1e49cddc1793ed5a93f1f1e0cdd89100b83f8c25
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-27 22:30:11 +0200

    Set child button of Thing to have color: inherit so it gets color from its parent Thing

--- src/components/thingWithChildren/Thing.jsx
+++ src/components/thingWithChildren/Thing.jsx
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 const Thing = styled.div`
   color: blue;
 
+  button {
+    color: inherit;
+  }
+
   .something {
     border: 1px solid; // an element labeled ".something" inside <Thing>
     display: block;
