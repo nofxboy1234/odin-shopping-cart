0c88a05617514b8c6906c6a81e0b1b902ac4e21b
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-28 16:59:49 +0200

    Add test using 'toBeInTheDocument()'

--- tests/home/Home.test.jsx
+++ tests/home/Home.test.jsx
@@ -3,6 +3,13 @@ import { render, screen } from '@testing-library/react';
 import Home from '../../src/components/home/Home';
 
 describe('Home component', () => {
+  it('has the heading in the document', () => {
+    render(<Home />);
+    expect(
+      screen.getByRole('heading', { name: 'Welcome to Shopping Cart!' })
+    ).toBeInTheDocument();
+  });
+
   it('renders correct heading', () => {
     render(<Home />);
     expect(
