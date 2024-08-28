1c452d6c7784360a8e22cfd1ccc2c2a7db9eedcd
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-28 16:29:45 +0200

    Update Home test

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <>
       <h1 className={styles.heading}>Welcome to Shopping Cart!</h1>
+      <h1>hello</h1>
     </>
   );
 };
--- tests/home/Home.test.jsx
+++ tests/home/Home.test.jsx
@@ -5,8 +5,9 @@ import Home from '../../src/components/home/Home';
 describe('Home component', () => {
   it('renders correct heading', () => {
     render(<Home />);
-    expect(screen.getByRole('heading').textContent).toMatch(
-      /Welcome to Shopping Cart!/i
-    );
+    expect(
+      screen.getByRole('heading', { name: 'Welcome to Shopping Cart!' })
+        .textContent
+    ).toMatch(/Welcome to Shopping Cart!/i);
   });
 });
