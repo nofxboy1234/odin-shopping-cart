68dcc68a6aae44bdcbaaedc74a6bb2b469baa6b8
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-29 12:36:34 +0200

    Update Shop test

--- tests/shop/Shop.test.jsx
+++ tests/shop/Shop.test.jsx
@@ -1,10 +1,13 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Shop from '../../src/components/shop/Shop';
+import App from '../../src/App';
 
 describe('Shop component', () => {
   it('renders the title', () => {
-    render(<Shop />);
+    render(<App />);
+    const paragraph = screen.getByRole('paragraph');
+    expect(paragraph.textContent).toMatch(/Loading.../i);
   });
 
   it('renders products', () => {});
