afec3a89958363e43864dec61c9b0d7dd62f3bdc
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-29 10:05:19 +0200

    Add test for Navigation

---
+++ tests/navigation/Navigation.test.jsx
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Navigation from '../../src/components/navigation/Navigation';
+
+describe.skip('Navigation component', () => {
+  it('renders Home, Shop, Cart links', () => {});
+});
