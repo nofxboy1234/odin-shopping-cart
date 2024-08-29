99621a5af01712c03f4af5872e36f93f627bc452
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-29 17:35:47 +0200

    Update Navigation test

--- tests/navigation/Navigation.test.jsx
+++ tests/navigation/Navigation.test.jsx
@@ -1,7 +1,32 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import Navigation from '../../src/components/navigation/Navigation';
+import routes from '../../src/routes/routes';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 
-describe.skip('Navigation component', () => {
-  it('renders Home, Shop, Cart links', () => {});
+describe('Navigation component', () => {
+  it('renders the Home link', async () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ['/'],
+      initialIndex: 0,
+    });
+
+    render(<RouterProvider router={router} />);
+
+    const link = await screen.findByRole('link', { name: 'Home' });
+    expect(link).toBeInTheDocument();
+  });
+
+  it('renders the Home heading', async () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ['/'],
+      initialIndex: 0,
+    });
+
+    render(<RouterProvider router={router} />);
+
+    const heading = await screen.findByRole('heading', {
+      name: 'Welcome to Shopping Cart!',
+    });
+    expect(heading).toBeInTheDocument();
+  });
 });
