76b81a3be07a376665666230e472f0c3ffe5a11a
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-29 10:07:35 +0200

    Add test for App

---
+++ tests/app/App.test.jsx
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import App from '../../src/App';
+
+describe.skip('App component', () => {
+  it('renders Navigation and Home/Shop/Cart components', () => {});
+});
