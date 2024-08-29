3eccc11c9e177a20144522d8b52605761dd43ac5
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-29 13:38:21 +0200

    Use createMemoryRouter to find Home link in Navigation bar

--- tests/shop/Shop.test.jsx
+++ tests/shop/Shop.test.jsx
@@ -1,13 +1,28 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import Shop from '../../src/components/shop/Shop';
 import App from '../../src/App';
+import Shop from '../../src/components/shop/Shop';
+import routes from '../../src/routes/routes';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 
 describe('Shop component', () => {
-  it('renders the title', () => {
+  it('renders the loading message', () => {
     render(<App />);
     const paragraph = screen.getByRole('paragraph');
     expect(paragraph.textContent).toMatch(/Loading.../i);
+  });
+
+  it('renders the Home link', async () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ['/'],
+      initialIndex: 0,
+    });
+
+    render(<RouterProvider router={router} />);
+    // render(<App />);
+
+    const link = await screen.findByRole('link', { name: 'Home' });
+    expect(link).toBeInTheDocument();
   });
 
   it('renders products', () => {});
