b35a79814605b0318ccdb42249f499de71a0e5f9
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-30 14:29:12 +0200

    Update Navigation tests to use setup function
    https://testing-library.com/docs/user-event/intro/#writing-tests-with-userevent
    "We discourage rendering or using any userEvent functions outside of the test itself - e.g. in a before/after hook..."

--- tests/navigation/Navigation.test.jsx
+++ tests/navigation/Navigation.test.jsx
@@ -3,27 +3,37 @@ import { render, screen } from '@testing-library/react';
 import routes from '../../src/routes/routes';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 
-beforeEach(() => {
-  const router = createMemoryRouter(routes, {
-    initialEntries: ['/'],
-    initialIndex: 0,
-  });
+function setup() {
+  return {
+    renderWithRouter: () => {
+      const router = createMemoryRouter(routes, {
+        initialEntries: ['/'],
+        initialIndex: 0,
+      });
 
-  render(<RouterProvider router={router} />);
-});
+      render(<RouterProvider router={router} />);
+    },
+  };
+}
 
 describe('Navigation component', () => {
   it('renders the home link', async () => {
+    const { renderWithRouter } = setup();
+    renderWithRouter();
     const link = await screen.findByRole('link', { name: 'Home' });
     expect(link).toBeInTheDocument();
   });
 
   it('renders the shop link', async () => {
+    const { renderWithRouter } = setup();
+    renderWithRouter();
     const link = await screen.findByRole('link', { name: 'Shop' });
     expect(link).toBeInTheDocument();
   });
 
   it('renders the cart link', async () => {
+    const { renderWithRouter } = setup();
+    renderWithRouter();
     const link = await screen.findByRole('link', { name: 'Cart (0)' });
     expect(link).toBeInTheDocument();
   });
