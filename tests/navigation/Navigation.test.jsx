fd75812634316413abcc82fa876fd891fc6d4d54
Dylan Palmboom <nofxboy1234@gmail.com>
2024-08-30 10:15:16 +0200

    Refactor Navigation tests to use beforeEach

--- tests/navigation/Navigation.test.jsx
+++ tests/navigation/Navigation.test.jsx
@@ -1,41 +1,29 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import routes from '../../src/routes/routes';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 
+beforeEach(() => {
+  const router = createMemoryRouter(routes, {
+    initialEntries: ['/'],
+    initialIndex: 0,
+  });
+
+  render(<RouterProvider router={router} />);
+});
+
 describe('Navigation component', () => {
   it('renders the home link', async () => {
-    const router = createMemoryRouter(routes, {
-      initialEntries: ['/'],
-      initialIndex: 0,
-    });
-
-    render(<RouterProvider router={router} />);
-
     const link = await screen.findByRole('link', { name: 'Home' });
     expect(link).toBeInTheDocument();
   });
 
   it('renders the shop link', async () => {
-    const router = createMemoryRouter(routes, {
-      initialEntries: ['/'],
-      initialIndex: 0,
-    });
-
-    render(<RouterProvider router={router} />);
-
     const link = await screen.findByRole('link', { name: 'Shop' });
     expect(link).toBeInTheDocument();
   });
 
   it('renders the shop link', async () => {
-    const router = createMemoryRouter(routes, {
-      initialEntries: ['/'],
-      initialIndex: 0,
-    });
-
-    render(<RouterProvider router={router} />);
-
     const link = await screen.findByRole('link', { name: 'Cart (0)' });
     expect(link).toBeInTheDocument();
   });
