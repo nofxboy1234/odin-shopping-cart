6ff213ad9acc2596d95101ae158e6395638d9994
Dylan Palmboom <nofxboy1234@gmail.com>
2024-09-03 15:35:52 +0200

    Add a snapshot test to Navigation tests

--- tests/navigation/Navigation.test.jsx
+++ tests/navigation/Navigation.test.jsx
@@ -1,5 +1,11 @@
 import { describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import {
+  getByText,
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import routes from '../../src/routes/routes';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 
@@ -11,7 +17,7 @@ function setup() {
         initialIndex: 0,
       });
 
-      render(<RouterProvider router={router} />);
+      return render(<RouterProvider router={router} />);
     },
   };
 }
@@ -48,5 +54,17 @@ describe('Navigation component', () => {
       { timeout: 2000 }
     );
     expect(link).toBeInTheDocument();
+  });
+
+  it('renders the navigation bar above the home page', async () => {
+    const { renderWithRouter } = setup();
+    const { container } = renderWithRouter();
+
+    // await waitForElementToBeRemoved(() => screen.getByText('Loading...'));
+    await waitFor(() => {
+      expect(screen.getByText('Welcome to Shopping Cart!')).toBeInTheDocument();
+    });
+    screen.debug();
+    expect(container).toMatchSnapshot();
   });
 });
