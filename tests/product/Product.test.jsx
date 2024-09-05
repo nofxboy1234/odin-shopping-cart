2b53123bf75d3f683c4acfb6ca5b7adcc4853542
Dylan Palmboom <nofxboy1234@gmail.com>
2024-09-05 09:06:05 +0200

    Setup Mock Service Worker and add test to Product

--- tests/product/Product.test.jsx
+++ tests/product/Product.test.jsx
@@ -1,7 +1,8 @@
-import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { screen } from '@testing-library/react';
 
 import renderWithRouter from '../helpers/router';
+import userEvent from '@testing-library/user-event';
 
 function setup() {
   return {
@@ -11,34 +12,7 @@ function setup() {
 }
 
 describe('Product component with API mocked', () => {
-  const productsMockValue = [
-    {
-      id: 1,
-      image: '',
-      title: 'a product',
-      price: 99.99,
-      quantity: 0,
-    },
-  ];
-
-  beforeEach(async () => {
-    vi.spyOn(window, 'fetch');
-  });
-
-  afterEach(() => {
-    vi.restoreAllMocks();
-  });
-
   it('renders an image of a product', async () => {
-    window.fetch.mockImplementation(() => {
-      const response = {
-        json: () => Promise.resolve(productsMockValue),
-        ok: true,
-      };
-
-      return Promise.resolve(response);
-    });
-
     const { renderWithRouter, path } = setup();
     renderWithRouter(path);
 
@@ -56,33 +30,46 @@ describe('Product component with API mocked', () => {
     expect(title).toBeInTheDocument();
   });
 
-  it.skip('renders the price of the product', () => {
-    vi.mocked(useProducts).mockReturnValue(productsMockValue);
-
+  it('renders the price of the product', async () => {
     const { renderWithRouter, path } = setup();
     renderWithRouter(path);
 
-    const price = screen.getByText('Price: 99.99');
+    const price = await screen.findByText('Price: 99.99');
     expect(price).toBeInTheDocument();
   });
 
-  it.skip('renders the initial quantity to add to the cart', () => {
-    vi.mocked(useProducts).mockReturnValue(productsMockValue);
-
+  it('renders the initial quantity to add to the cart', async () => {
     const { renderWithRouter, path } = setup();
     renderWithRouter(path);
 
-    const quantity = screen.getByDisplayValue('1');
+    const quantity = await screen.findByDisplayValue('1');
     expect(quantity).toBeInTheDocument();
   });
 
-  it.skip('renders a button to add to the cart', () => {
-    vi.mocked(useProducts).mockReturnValue(productsMockValue);
-
+  it('renders a button to add to the cart', async () => {
     const { renderWithRouter, path } = setup();
     renderWithRouter(path);
 
-    const button = screen.getByRole('button', { name: 'Add to Cart' });
+    const button = await screen.findByRole('button', { name: 'Add to Cart' });
     expect(button).toBeInTheDocument();
+  });
+
+  describe.only('when clicking the Add to Cart button', () => {
+    it('updates the cart navigation menu quantity', async () => {
+      const user = userEvent.setup();
+
+      const { renderWithRouter, path } = setup();
+      renderWithRouter(path);
+
+      const button = await screen.findByRole('button', { name: 'Add to Cart' });
+      const link = await screen.findByRole('link', {
+        name: 'Cart (0)',
+      });
+      expect(link).toBeInTheDocument();
+      await user.click(button);
+      expect(link).toHaveTextContent('Cart (1)');
+
+      screen.debug();
+    });
   });
 });
